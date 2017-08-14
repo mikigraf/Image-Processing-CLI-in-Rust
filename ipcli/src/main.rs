da2f3ff9b5c83048b05ac1334f16104d749b9a0e
Mikołaj Wawrzyniak <mikolajspejs@gmail.com>
2017-08-14 16:08:47 +0200

    Added thumbnail operation and removed unused dependencies

--- ipcli/src/main.rs
+++ ipcli/src/main.rs
@@ -1,15 +1,23 @@
 extern crate clap;
 extern crate image;
+
 use clap::{Arg,App,SubCommand};
 use std::fs::File;
 use image::{FilterType, GenericImage, Pixel};
 use image::DynamicImage;
+use std::path::Path;
 
 fn main() {
      let matches = App::new("IPCLI")
                           .version("0.1")
                           .author("Mikolaj Wawrzyniak <mikolaj.wawrzyniak at fh-dortmund.de>")
                           .about("Basic CLI for image processing")
+                          .arg(Arg::with_name("operation")
+                               .short("o")
+                               .long("operation")
+                               .help("Specifies operation to be done on the image")
+                               .takes_value(true)
+                               .required(true))
                           .arg(Arg::with_name("image")
                                .short("i")
                                .long("image")
@@ -21,7 +29,7 @@ fn main() {
                                .short("s")
                                .long("save")
                                .value_name("FILE")
-                               .help("Outputs transformed image into specified file.")
+                               .help("Outputs transformed image into specified file OR uses the second file to calculate the perceptual hash values and outputs their difference")
                                .takes_value(true)
                                .required(true))
                           .get_matches();
@@ -33,7 +41,15 @@ fn main() {
     let savePath = matches.value_of("save").unwrap_or("empty");
     println!("Into: {}", savePath);
 
-    openAndSave(imagePath, savePath);
+    let operation = matches.value_of("operation").unwrap_or("empty");
+    println!("Operation: {}", operation);
+
+    match operation.as_ref(){
+        "copy" => {openAndSave(imagePath, savePath)}
+        "thumbnail" => {createThumnbail(imagePath, savePath)}
+        _ => {println!("Not implemented yet!")}
+    }
+    //openAndSave(imagePath, savePath);
     // Vary the output based on how many times the user used the "verbose" flag
     // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
     match matches.occurrences_of("v") {
@@ -41,6 +57,19 @@ fn main() {
         1 => println!("Some verbose info"),
         2 => println!("Tons of verbose info"),
         3 | _ => println!("Don't be crazy"),
+    }
+}
+
+fn createThumnbail(i: &str, s: &str){
+    let img = image::open(i).expect("Opening image failed");
+    // get the type of the image
+    let thumbnail = img.resize(120,120, FilterType::Lanczos3);
+    let ext: String = i.chars().skip(i.len()-3).take(3).collect();
+    let mut out = File::create(s).unwrap();
+    match ext.as_ref() {
+        "jpg" => {thumbnail.save(&mut out, image::JPEG).expect("Saving image failed");}
+        "png" => {thumbnail.save(&mut out, image::PNG).expect("Saving image failed");}
+        _ => {println!("something else")}
     }
 }
 
